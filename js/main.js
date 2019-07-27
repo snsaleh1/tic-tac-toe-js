bcf07db2bcb6468d4426a02fe3959982b40aae40
Caleb Saleh <calebsaleh@Calebs-MacBook-Pro.local>
2019-07-27 15:11:46 -0500

    save title

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log(hi);
+/*----- constants -----*/
+const PLAYERS = {
+    '0': 'white',
+    '1': 'red',
+    '-1': 'grey'
+  };
+  
+  /*----- app's state (variables) -----*/ 
+  let board, turn, winner;
+  
+  /*----- cached element references -----*/
+  const msgEl = document.getElementById('msg');
+  
+  /*----- event listeners -----*/ 
+//   document.querySelector('section.markers')
+//     .addEventListener('click', handleClick);
+  
+  /*----- functions -----*/
