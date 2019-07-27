6c103750c835deb3c7ca22c6fe9a18f13bca2d72
Caleb Saleh <calebsaleh@Calebs-MacBook-Pro.local>
2019-07-27 18:17:59 -0500

    Board is set

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
-/*----- constants -----*/
-const PLAYERS = {
-    '0': 'white',
-    '1': 'red',
-    '-1': 'grey'
-  };
-  
-  /*----- app's state (variables) -----*/ 
-  let board, turn, winner;
-  
-  /*----- cached element references -----*/
-  const msgEl = document.getElementById('msg');
-  
-  /*----- event listeners -----*/ 
-//   document.querySelector('section.markers')
-//     .addEventListener('click', handleClick);
-  
-  /*----- functions -----*/
+let gameBoard;
+let PLAYERONE = 'O';
+let PLAYERTWO = 'X';
+
+const winnerPlay = [
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ];
+
+const cells = document.getElementsByClassName('cell');
+onStartGame();
+
+function onStartGame(){
+    document.querySelector('.end-game').style.display = 'none';
+    gameBoard = Array.from(Array(9).keys());
+    for(let i=0; i< cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', onTurnClick, false)
+      }
+    };
