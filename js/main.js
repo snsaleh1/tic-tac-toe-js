7e4278a8994f1eb7c37e043f6b0d08eed57ca3c2
snsaleh1 <snsaleh11@gmail.com>
2019-07-28 15:16:02 -0500

    computer player two functional

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //set variables
 var gameBoard;
-const PLAYERONE = 'O';
-const PLAYERTWO = 'X';
+const playerOne = 'O';
+const playerTwo = 'X';
 
 const winnerPlay = [
     [0, 4, 8],
@@ -28,10 +28,64 @@ function startGame() {
     }
 }
 function turnClick(square) {
-  turnClick(square.target.id, PLAYERONE)
+  if (typeof gameBoard[square.target.id] == 'number') {
+    turn(square.target.id, playerOne)
+    if (!checkTie()) turn(bestSpot(), playerTwo);
+  }
 }
 
 function turn(squareId, player) {
   gameBoard[squareId] = player;
   document.getElementById(squareId).innerText = player;
+  let gameWinner = checkWinner(gameBoard, player)
+  if (gameWinner) gameOver(gameWinner)
+}
+
+function checkWinner(board, player) {
+  let plays = board.reduce((a, e, i) => 
+  (e === player) ? a.concat(i) : a, []);
+  let gameWinner = null;
+  for (let [index,win] of winnerPlay.entries()) {
+    if(win.every(elem => plays.indexOf(elem) > -1)) {
+        gameWinner = {index: index, player: player};
+        break;
+  }
+}
+  return gameWinner;
+}
+
+function gameOver(gameWinner) {
+  for (let index of winnerPlay[gameWinner.index]) {
+    document.getElementById(index). style.backgroundColor = 
+    gameWinner.player == playerOne ? "green" : "yellow";
+  }
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener('click', turnClick, false)
+  }
+  declareWinner(gameWinner.player == playerOne ? "Well done, you beauty!" : "Dammit kid you blew it!");
+}
+
+function declareWinner(who) {
+  document.querySelector(".endgame").display = "block";
+  document.querySelector("endgame. text").innerText = who;
+}
+
+function emptySquares() {
+  return gameBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+  return emptySquares()[0]
+}
+
+function checkTie() {
+  if (emptySquares(). length ==0) {
+    for (var i = 0; i <cells.length; i++) {
+      cells [i].style.backgroundColor = "blue";
+      cells [i].removeEventListener('click', turnClick, false);
+    }
+    declareWinner("DRAW!")
+    return true;
+  }
+  return false;
 }
